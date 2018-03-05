8b85edc2653b38cc89bb0d3bdd18c2e067a7b0b1
bigwanggang <bigwanggang@163.com>
2018-03-05 17:26:36 +0800

    Create ShotPlaneApplication.java

---
+++ src/main/java/com/bigwanggang/ShotPlaneApplication.java
@@ -0,0 +1,12 @@
+package com.bigwanggang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShotPlaneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShotPlaneApplication.class, args);
+    }
+
+}
