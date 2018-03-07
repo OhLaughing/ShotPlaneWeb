0a5170787eb83db8352ace8b8aeb6f93655959ac
bigwanggang <bigwanggang@163.com>
2018-03-07 22:18:07 +0800

    add files

---
+++ src/main/java/com/bigwanggang/Util.java
@@ -0,0 +1,10 @@
+package com.bigwanggang;
+
+/**
+ * Created by gustaov on 2018/3/7.
+ */
+public class Util {
+    public static Point getPoint(String s) {
+        return new Point(1,1);
+    }
+}
