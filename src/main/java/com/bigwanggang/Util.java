f0763e39e0fc57e3c050ff36e4592f9b947c7c76
bigwanggang <bigwanggang@163.com>
2018-03-08 21:16:24 +0800

    add files

--- src/main/java/com/bigwanggang/Util.java
+++ src/main/java/com/bigwanggang/Util.java
@@ -7,10 +7,11 @@ import java.util.regex.Pattern;
  * Created by gustaov on 2018/3/7.
  */
 public class Util {
-    public static final Pattern p = Pattern.compile("x:(\\d+),y:(\\d+)");
+    public static final Pattern POINTPATTERN = Pattern.compile("x:(\\d+),y:(\\d+)");
+    public static final Pattern POSITIONPATTERN = Pattern.compile("p:(\\d+)");
 
     public static Point getPoint(String s) {
-        Matcher m = p.matcher(s);
+        Matcher m = POINTPATTERN.matcher(s);
         int x = -1;
         int y = -1;
         if(m.find()) {
@@ -18,5 +19,13 @@ public class Util {
             y = Integer.parseInt(m.group(2));
         }
         return new Point(x, y);
+    }
+    public static int getPosition(String s) {
+        Matcher m = POSITIONPATTERN.matcher(s);
+        int p = -1;
+        if(m.find()) {
+            p = Integer.parseInt(m.group(1));
+        }
+        return p;
     }
 }
