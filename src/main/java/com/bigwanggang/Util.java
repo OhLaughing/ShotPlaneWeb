4add9dde4767dcacf51d469c717e965823982932
bigwanggang <bigwanggang@163.com>
2018-03-08 08:27:28 +0800

    Update Util.java

--- src/main/java/com/bigwanggang/Util.java
+++ src/main/java/com/bigwanggang/Util.java
@@ -1,10 +1,22 @@
 package com.bigwanggang;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by gustaov on 2018/3/7.
  */
 public class Util {
+    public static final Pattern p = Pattern.compile("x:(\\d+),y:(\\d+)");
+
     public static Point getPoint(String s) {
-        return new Point(1,1);
+        Matcher m = p.matcher(s);
+        int x = -1;
+        int y = -1;
+        if(m.find()) {
+            x = Integer.parseInt(m.group(1));
+            y = Integer.parseInt(m.group(2));
+        }
+        return new Point(x, y);
     }
 }
